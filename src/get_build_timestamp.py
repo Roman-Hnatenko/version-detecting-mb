b2b15d4633568243e7715cb035f95f5be840a525
Roman Hnatenko <hnatenkoroman@gmail.com>
2022-07-20 17:23:14 +0300

    Add get_build_timestamp logic

---
+++ src/get_build_timestamp.py
@@ -0,0 +1,39 @@
+import re
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+GAME_URL = 'https://www.maxbet.ro/ro/jocuri-pacanele-online/vampire-night-egt'
+
+
+def get_build_timestamp(driver: WebDriver) -> str:
+    waiter = WebDriverWait(driver, 5)
+    main_window = driver.current_window_handle
+
+    outer_frame = waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ui-one-games-container > iframe')))
+    driver.switch_to.frame(outer_frame)
+    driver.switch_to.frame(driver.find_element(By.ID, 'ifmGame'))
+
+    inner_frame = driver.find_element(By.CSS_SELECTOR, 'iframe')
+    src_link = inner_frame.get_attribute('src')
+    match = re.search(r'buildTimestamp=(\d+)', src_link)
+    print(match)
+    driver.switch_to.window(main_window)
+    return match[1]
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get(GAME_URL)
+    try:
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.modal-footer > .btn.btn-red-restyle'))).click()  # Accept notification
+    except TimeoutException:
+        pass
+    driver.implicitly_wait(5)
+    get_build_timestamp(driver)
--- src/main.py
+++ src/main.py
@@ -1,2 +1,46 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+URL = 'https://www.maxbet.ro/ro/cazino-online/egtgames'
+
+
+def load_all_content(driver: WebDriver):
+    waiter = WebDriverWait(driver, 5)
+    try:
+        waiter.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.modal-footer > .btn.btn-red-restyle'))).click()  # Accept notification
+    except TimeoutException:
+        pass
+    while True:
+        try:
+            waiter.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.item-img-arrow-down'))).click()
+        except TimeoutException:
+            break
+        except Exception:
+            pass
+
+
+def handle_game(driver: WebDriver):
+    waiter = WebDriverWait(driver, 8)
+    game = waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.game')))
+    ActionChains(driver).move_to_element(game).perform()  # place the cursor
+    driver.implicitly_wait(5)
+    driver.execute_script("document.getElementsByClassName('ui-bottom-inner')[0].click()")  # TO DO
+
+
+def main(driver: WebDriver):
+    load_all_content(driver)
+    handle_game(driver)
+
+
 if __name__ == '__main__':
-    pass
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get(URL)
+    driver.implicitly_wait(5)
+    main(driver)
